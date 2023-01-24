e76d962cae8c58c0df8898669d4268cf170b25b3
trindadematheus97 <trindadedt3@gmail.com>
2023-01-24 17:34:14 -0300

    Espectador commit

--- Cinema.Core/Entities/Poltrona.cs
+++ Cinema.Core/Entities/Poltrona.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public int PoltronaSala { get; set; }
-        public bool PoltronaOcupada { get; set; }
+        public bool PoltronaLivre { get; set; }
         public int SalaId { get; set; }
         public Sala Sala { get; set; }
     }     
